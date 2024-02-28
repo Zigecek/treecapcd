51df24d572b6cb3d9e52143c2a754d33d8fde932
Jan Kozohorský <honza007cz@hotmail.com>
2024-02-28 05:21:56 +0100

    init

---
+++ src/main/java/zige/treecap/treecapcd/client/TreecapcdClient.java
@@ -0,0 +1,61 @@
+package zige.treecap.treecapcd.client;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.ActionResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.*;
+
+@Environment(EnvType.CLIENT)
+public class TreecapcdClient implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("treecapcd");
+    private boolean isCoolDown = false;
+    private final AttackBlockCallback attackBlockCallback  = (player, world, hand, pos, direction) -> {
+        if (isCoolDown) return ActionResult.PASS;
+
+        NbtCompound handStackNbt = player.getMainHandStack().getNbt();
+        if (handStackNbt == null) return ActionResult.PASS;
+        String handStackName = handStackNbt.getCompound("ExtraAttributes").getString("id");
+        if (handStackName == null) return ActionResult.PASS;
+
+        if (handStackName.equals("TREECAPITATOR_AXE")) {
+            new Thread(() -> {
+                while (!world.getBlockState(pos).isAir()) {
+                }
+                isCoolDown = true;
+                new Thread(() -> {
+                    try {
+                        Thread.sleep(2000);
+                        isCoolDown = false;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            }).start();
+        }
+        return ActionResult.PASS;
+    };
+    private final HudRenderCallback hudRenderCallback  = (e, phases) -> {
+        if (isCoolDown) {
+            e.fillGradient(
+                    0,
+                    0,
+                    e.getScaledWindowWidth(),
+                    (int) (e.getScaledWindowHeight() * 0.045f),
+                    Color.RED.getRGB(),
+                    Color.TRANSLUCENT);
+        }
+    };
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Treecapcd client initialized");
+        AttackBlockCallback.EVENT.register(attackBlockCallback);
+        HudRenderCallback.EVENT.register(hudRenderCallback);
+    }
+}
